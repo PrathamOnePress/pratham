d80ddab989ec12a68f9ec12ad4ad9df6f43e72a2
ilovegorakhpur <jawahar.mallah@gmail.com>
2025-12-03 19:43:50 +0530

    Updated Old
    Updated Old

--- components/Header.js
+++
@@ -1,8 +0,0 @@
-
-import Link from 'next/link';
-export default function Header(){ return (
-  <header style={{padding:20,display:'flex',justifyContent:'space-between',alignItems:'center'}}>
-    <div style={{fontWeight:600}}>PrathamOne</div>
-    <nav><Link href='/'>Home</Link> | <Link href='/contact'>Contact</Link></nav>
-  </header>
-) }
